af20fd2e5227c08fe04d8f67a252dedbb16c00fa
magesh <hariarunengg@gmail.com>
2022-05-31 07:04:22 +0530

    server setuped 😀

---
+++ server/src/server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const PORT = process.env.PORT || 8000;
+
+const app = require("./app");
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log("listen");
+});
+
+console.log(PORT);
